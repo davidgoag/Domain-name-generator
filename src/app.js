2ccbc93d6361766fdc9b13494150abeb860571d3
David Gómez <davidgoag1@gmail.com>
2021-08-31 01:08:36 +0000

    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let four = [".com", ".net", ".us"];
+
+  let string = "";
+
+  for (let pronouns of pronoun) {
+    for (let adjs of adj) {
+      for (let nous of noun) {
+        for (let fours of four) {
+          document.querySelector("#ola").innerHTML +=
+            "<li>" + "www." + pronouns + adjs + nous + fours + "</li>";
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
